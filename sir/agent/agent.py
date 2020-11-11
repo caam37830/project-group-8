f4dccb6ea2a5858d4360be35544e0b8fed454d1d
jackpotrykus <jackpotrykus@gmail.com>
2020-11-11 15:18:11 -0600

    added step_days() method

--- sir/agent/agent.py
+++ sir/agent/agent.py
@@ -1,9 +1,10 @@
 import random
 import math
+import numpy as np
 
 
 class SIRModel:
-    def __init__(self, b, k, size, prob_infect=1):
+    def __init__(self, b, k, size, prob_infect=1, initial_infect=None):
         """
         Initialize an `SIRModel` class
         :param b: number of interactions per day, per agent, which could result in infection
@@ -11,21 +12,22 @@ class SIRModel:
         :param size: number of agents to generate
         :param prob_infect: (optional) probability that an interaction between a susceptible
         agent and an infected agent results in the susceptible agent's infection
+        :param initial_infect: (optional) if supplied, start with `initial_infect` agents already infected
         """
-        self.b = b
-        self.k = k
-        self.size = size
+        self.b, self.k, self.size = b, k, size
         self.agents = [Agent(ii) for ii in range(size)]
         self.susceptible = [agent.id for agent in self.agents]
         self.infected = []
         self.recovered = []
         self.days_passed = 0
         self.prob_infect = prob_infect
+        self.initial_infect = initial_infect
+        if self.initial_infect is not None:
+            self.exogenous_infect(n=initial_infect)
 
     def exogenous_infect(self, n=None, indices=None):
         """
-        Infect `n` of the individuals in `self.population` exogenously (i.e., outside
-        model parameters)
+        Infect `n` of the individuals in `self.population` exogenously (i.e., outside model parameters)
         :param n: Number of agents to infect
         :param indices: Alternative to `n`, specify the indices in `self.agents` to infect
         :return: None
@@ -34,9 +36,8 @@ class SIRModel:
             if n <= len(self.susceptible):
                 infected = sorted(random.sample(self.susceptible, k=n))
                 for agent_id in infected:
-                    self.susceptible.remove(agent_id)
-                    self.infected.append(agent_id)
                     self.agents[agent_id].infect()
+                    self.categorize_agents()
             else:
                 print(
                     "SIRModel.exogenous_infect: `n` greater than the number of susceptible agents"
@@ -45,9 +46,8 @@ class SIRModel:
         if indices is not None:
             if set(indices).issubset(set(self.susceptible)):
                 for agent_id in indices:
-                    self.susceptible.remove(agent_id)
-                    self.infected.append(agent_id)
                     self.agents[agent_id].infect()
+                    self.categorize_agents()
             else:
                 print(
                     "SIRModel.exogenous_infect: `indices` contains non-susceptible agents"
@@ -58,9 +58,10 @@ class SIRModel:
                 "SIRModel.exogenous_infect: supply either `n` or `indices`. No action was taken"
             )
 
+
     def reset(self):
         """
-        Reset the model to its initial state
+        Reset the model to a "clean slate"
         :return: None
         """
         for agent in self.agents:
@@ -91,14 +92,16 @@ class SIRModel:
     def step(self):
         """
         Simulate one day according to SIR model parameters
-        :return:
+        :return: None
         """
         agent_ids = [agent.id for agent in self.agents]
+
         # Recover k proportion of the infected
-        num_recover = math.floor(len(self.infected) * self.k)
+        num_recover = math.ceil(len(self.infected) * self.k)
         ids_recover = random.sample(self.infected, k=num_recover)
         for r_id in ids_recover:
             self.agents[r_id].recover()
+            self.categorize_agents()
 
         # Infect susceptible agents, if they meet an infected agent
         for i_id in self.infected:
@@ -115,14 +118,41 @@ class SIRModel:
         self.categorize_agents()
         self.days_passed += 1
 
+    def step_t_days(self, days):
+        """
+        Simulate infections for `days` according to the procedure defined in `step`
+        :param days: Number of days to step
+        :return: `days` by `4` numpy array, with columns ...
+          - day number
+          - number susceptible
+          - number infected
+          - number recovered
+        """
+        num_d = np.zeros(days, dtype=np.int64)  # day indices
+        num_s = np.zeros(days, dtype=np.int64)  # num susceptible
+        num_i = np.zeros(days, dtype=np.int64)  # num infected
+        num_r = np.zeros(days, dtype=np.int64)  # num recovered
+
+        # Initialize the 0th index to the initial state of the model
+        num_d[0], num_s[0], num_i[0], num_r[0] = self.summarize_model()
+
+        for ii in np.arange(1, days):
+            self.step()
+            num_d[ii], num_s[ii], num_i[ii], num_r[ii] = self.summarize_model()
+
+        return np.array([num_d, num_s, num_i, num_r]).T
+
     def summarize_model(self):
         """
+        Summarize the current state of the `SIRModel` object
         :return: A tuple summarizing the state of the model
         """
-        num_susceptible = len(self.susceptible)
-        num_infected = len(self.infected)
-        num_recovered = len(self.recovered)
-        return self.days_passed, num_susceptible, num_infected, num_recovered
+        return (
+            self.days_passed,
+            len(self.susceptible),
+            len(self.infected),
+            len(self.recovered),
+        )
 
 
 class Agent:
